1ce0968ccc8ab1dc7c4444c891da5b824276c653
Amarir <amarirsafa@gmail.com>
2020-05-20 19:22:09 +0000

    Items display correctly and upload correctly, itemsdetails not linked to the data base yet

--- app/src/main/java/com/example/mystore/ItemDetailsActivity.java
+++ app/src/main/java/com/example/mystore/ItemDetailsActivity.java
@@ -1,24 +1,45 @@
 package com.example.mystore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class ItemDetailsActivity extends AppCompatActivity {
     private Item item;
     private ImageView imageView;
-    private TextView item_title,item_price,item_description;
+    private TextView item_title, item_price, item_description;
+    private FirebaseAuth userAuth;
+    private Button Add_to_cart;
+    private CollectionReference userRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
 
+        userAuth = FirebaseAuth.getInstance();
+        userRef = FirebaseFirestore.getInstance().collection("users");
+
+
+        Add_to_cart = findViewById(R.id.add_to_cart_button);
         imageView = findViewById(R.id.image_gallery);
         item_title = findViewById(R.id.product_name);
         item_price = findViewById(R.id.product_price);
@@ -26,15 +47,36 @@ public class ItemDetailsActivity extends AppCompatActivity {
 
 
         Intent intent = getIntent();
-        Item item = intent.getParcelableExtra("Item");
+        final Item item = intent.getParcelableExtra("Item");
 
         //imageView.setImageURI(Uri.parse(item.getPictures().get(0)));
         Picasso.get().load(item.getPictures().get(0)).fit().centerCrop().into(imageView);
         item_title.setText(item.getTitle());
         item_price.setText(item.getPrice().toString());
-        String item_description2 = item.getDescription()+"\n Item category: \n"+item.getCategory()+
-                "\n this item was made by: "+ item.getWho_ma_it()+"\n and it's :"+item.getWhat_is_it();
+        String item_description2 = item.getDescription() + "\n Item category: \n" + item.getCategory() +
+                "\n this item was made by: " + item.getWho_ma_it() + "\n and it's :" + item.getWhat_is_it();
         item_description.setText(item_description2);
+
+        Add_to_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userRef.document(Objects.requireNonNull(userAuth.getUid())).collection("products")
+                        .document(item.getId() + "")
+                        .set(item).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(ItemDetailsActivity.this, "The item has been successfully saved", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ItemDetailsActivity.this, "Something went wrong!ITEM NOT SAVED", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ;
+
+            }
+        });
 
     }
 }
