d1bb68772f7b6acc63cad3c31f8c6b7f3a56d918
Amarir <amarirsafa@gmail.com>
2020-07-22 18:20:49 +0100

    cart is done

--- app/src/main/java/com/example/mystore/Classes/Order.java
+++ app/src/main/java/com/example/mystore/Classes/Order.java
@@ -1,20 +1,22 @@
 package com.example.mystore.Classes;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Order {
     private Item itemToOrder;
     private User user;
+    private String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(Calendar.getInstance().getTime());
 
     public Order(){}
-    public Order(Item itemToOrder,User user){
-        setItemToOrder(itemToOrder);
-        setUser(user);
-    }
+
 
     public Item getItemToOrder() { return itemToOrder; }
     public void setItemToOrder(Item itemToOrder) { this.itemToOrder = itemToOrder; }
 
     public User getUser() { return user; }
     public void setUser(User user) { this.user = user; }
+
+    public String getCurrentDate() { return currentDate; }
 }
